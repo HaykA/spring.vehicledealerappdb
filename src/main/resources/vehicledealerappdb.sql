f3762030e3fd5712b7a508ab9aaae769530bf9ce
Hayk <hayk@live.be>
2016-05-17 13:59:09 +0200

    Sprint 1 - updating SQL for Hello World app

--- src/main/resources/vehicledealerappdb.sql
+++ src/main/resources/vehicledealerappdb.sql
@@ -29,17 +29,21 @@ CREATE TABLE `article` (
   `reference` varchar(12) NOT NULL,
   `title` varchar(255) NOT NULL,
   `created` date NOT NULL,
-  `creatorid` int(9) NOT NULL,
+  `creatorid` int(9) unsigned NOT NULL,
   `modified` date DEFAULT NULL,
-  `modifierid` int(9) DEFAULT NULL,
+  `modifierid` int(9) unsigned DEFAULT NULL,
   `state` enum('DEFAULT','NEW','USED') NOT NULL DEFAULT 'DEFAULT',
   `baseprice` decimal(10,0) NOT NULL DEFAULT '-1',
   `pricereduction` decimal(10,0) NOT NULL DEFAULT '0',
-  `branchid` int(9) NOT NULL,
-  `views` int(9) NOT NULL DEFAULT '0',
-  `currencyid` int(9) NOT NULL,
+  `branchid` int(9) unsigned NOT NULL,
+  `views` int(9) unsigned NOT NULL DEFAULT '0',
+  `description` varchar(2000) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`)
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `fk_article$branchid_idx` (`branchid`),
+  CONSTRAINT `fk_article$branch` FOREIGN KEY (`branchid`) REFERENCES `branch` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_article$creator` FOREIGN KEY (`id`) REFERENCES `auth_user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_article$modifier` FOREIGN KEY (`id`) REFERENCES `auth_user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -53,29 +57,6 @@ LOCK TABLES `article` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `article_category`
---
-
-DROP TABLE IF EXISTS `article_category`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `article_category` (
-  `articleid` int(9) unsigned NOT NULL,
-  `categoryid` int(9) unsigned NOT NULL,
-  PRIMARY KEY (`articleid`,`categoryid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `article_category`
---
-
-LOCK TABLES `article_category` WRITE;
-/*!40000 ALTER TABLE `article_category` DISABLE KEYS */;
-/*!40000 ALTER TABLE `article_category` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `article_person`
 --
 
@@ -85,7 +66,10 @@ DROP TABLE IF EXISTS `article_person`;
 CREATE TABLE `article_person` (
   `articleid` int(9) unsigned NOT NULL,
   `personid` int(9) unsigned NOT NULL,
-  PRIMARY KEY (`articleid`,`personid`)
+  PRIMARY KEY (`articleid`,`personid`),
+  KEY `fk_article_person$person_idx` (`personid`),
+  CONSTRAINT `fk_article_person$article` FOREIGN KEY (`articleid`) REFERENCES `article` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_article_person$person` FOREIGN KEY (`personid`) REFERENCES `person` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -99,6 +83,56 @@ LOCK TABLES `article_person` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `article_tag`
+--
+
+DROP TABLE IF EXISTS `article_tag`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `article_tag` (
+  `articleid` int(9) unsigned NOT NULL,
+  `tagid` int(9) unsigned NOT NULL,
+  PRIMARY KEY (`articleid`,`tagid`),
+  KEY `fk_article_category$categoryid_idx` (`tagid`),
+  CONSTRAINT `fk_article_tag$article` FOREIGN KEY (`articleid`) REFERENCES `article` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_article_tag$tag` FOREIGN KEY (`tagid`) REFERENCES `tag` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `article_tag`
+--
+
+LOCK TABLES `article_tag` WRITE;
+/*!40000 ALTER TABLE `article_tag` DISABLE KEYS */;
+/*!40000 ALTER TABLE `article_tag` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `auth_authority`
+--
+
+DROP TABLE IF EXISTS `auth_authority`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `auth_authority` (
+  `id` int(9) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `auth_authority`
+--
+
+LOCK TABLES `auth_authority` WRITE;
+/*!40000 ALTER TABLE `auth_authority` DISABLE KEYS */;
+/*!40000 ALTER TABLE `auth_authority` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `auth_user`
 --
 
@@ -107,7 +141,13 @@ DROP TABLE IF EXISTS `auth_user`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `auth_user` (
   `id` int(9) unsigned NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id`)
+  `personid` int(9) unsigned NOT NULL,
+  `username` varchar(45) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `enabled` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  UNIQUE KEY `personid_UNIQUE` (`personid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -121,6 +161,29 @@ LOCK TABLES `auth_user` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `auth_user_authority`
+--
+
+DROP TABLE IF EXISTS `auth_user_authority`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `auth_user_authority` (
+  `userid` int(9) unsigned NOT NULL,
+  `authorityid` int(9) unsigned NOT NULL,
+  PRIMARY KEY (`userid`,`authorityid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `auth_user_authority`
+--
+
+LOCK TABLES `auth_user_authority` WRITE;
+/*!40000 ALTER TABLE `auth_user_authority` DISABLE KEYS */;
+/*!40000 ALTER TABLE `auth_user_authority` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `branch`
 --
 
@@ -128,9 +191,16 @@ DROP TABLE IF EXISTS `branch`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `branch` (
-  `id` int(9) unsigned NOT NULL,
+  `id` int(9) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `street` varchar(45) DEFAULT NULL,
+  `house` varchar(10) DEFAULT NULL,
+  `apt` varchar(10) DEFAULT NULL,
+  `cityid` int(9) unsigned NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`)
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `fk_branch$city_idx` (`cityid`),
+  CONSTRAINT `fk_branch$city` FOREIGN KEY (`cityid`) REFERENCES `city` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -144,6 +214,32 @@ LOCK TABLES `branch` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `brand`
+--
+
+DROP TABLE IF EXISTS `brand`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `brand` (
+  `id` int(9) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `alt_name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  UNIQUE KEY `name_UNIQUE` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `brand`
+--
+
+LOCK TABLES `brand` WRITE;
+/*!40000 ALTER TABLE `brand` DISABLE KEYS */;
+/*!40000 ALTER TABLE `brand` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `category`
 --
 
@@ -151,11 +247,10 @@ DROP TABLE IF EXISTS `category`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `category` (
-  `id` int(9) NOT NULL AUTO_INCREMENT,
+  `id` int(9) unsigned NOT NULL,
   `name` varchar(45) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`),
-  UNIQUE KEY `name_UNIQUE` (`name`)
+  UNIQUE KEY `id_UNIQUE` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -166,6 +261,59 @@ CREATE TABLE `category` (
 LOCK TABLES `category` WRITE;
 /*!40000 ALTER TABLE `category` DISABLE KEYS */;
 /*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `city`
+--
+
+DROP TABLE IF EXISTS `city`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `city` (
+  `id` int(9) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `postalcode` varchar(45) DEFAULT NULL,
+  `countryid` int(9) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `fk_city$countryid_idx` (`countryid`),
+  CONSTRAINT `fk_city$country` FOREIGN KEY (`countryid`) REFERENCES `country` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `city`
+--
+
+LOCK TABLES `city` WRITE;
+/*!40000 ALTER TABLE `city` DISABLE KEYS */;
+/*!40000 ALTER TABLE `city` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `country`
+--
+
+DROP TABLE IF EXISTS `country`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `country` (
+  `id` int(9) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  UNIQUE KEY `name_UNIQUE` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `country`
+--
+
+LOCK TABLES `country` WRITE;
+/*!40000 ALTER TABLE `country` DISABLE KEYS */;
+/*!40000 ALTER TABLE `country` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -201,7 +349,11 @@ DROP TABLE IF EXISTS `person`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `person` (
   `id` int(9) unsigned NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id`)
+  `firstname` varchar(45) NOT NULL,
+  `secondname` varchar(45) NOT NULL,
+  `branchid` int(9) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -213,6 +365,129 @@ LOCK TABLES `person` WRITE;
 /*!40000 ALTER TABLE `person` DISABLE KEYS */;
 /*!40000 ALTER TABLE `person` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `tag`
+--
+
+DROP TABLE IF EXISTS `tag`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tag` (
+  `id` int(9) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  UNIQUE KEY `name_UNIQUE` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tag`
+--
+
+LOCK TABLES `tag` WRITE;
+/*!40000 ALTER TABLE `tag` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tag` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `vehicle`
+--
+
+DROP TABLE IF EXISTS `vehicle`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `vehicle` (
+  `articleid` int(9) unsigned NOT NULL,
+  `modelid` int(9) unsigned DEFAULT NULL,
+  `builddate` date DEFAULT NULL,
+  `compliancedate` date DEFAULT NULL,
+  `mileage` int(7) unsigned DEFAULT NULL,
+  PRIMARY KEY (`articleid`),
+  UNIQUE KEY `articleid_UNIQUE` (`articleid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `vehicle`
+--
+
+LOCK TABLES `vehicle` WRITE;
+/*!40000 ALTER TABLE `vehicle` DISABLE KEYS */;
+/*!40000 ALTER TABLE `vehicle` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `vehicle_model`
+--
+
+DROP TABLE IF EXISTS `vehicle_model`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `vehicle_model` (
+  `id` int(9) unsigned NOT NULL AUTO_INCREMENT,
+  `brandid` int(9) unsigned NOT NULL,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `vehicle_model`
+--
+
+LOCK TABLES `vehicle_model` WRITE;
+/*!40000 ALTER TABLE `vehicle_model` DISABLE KEYS */;
+/*!40000 ALTER TABLE `vehicle_model` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `vehicle_option`
+--
+
+DROP TABLE IF EXISTS `vehicle_option`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `vehicle_option` (
+  `id` int(9) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `vehicle_option`
+--
+
+LOCK TABLES `vehicle_option` WRITE;
+/*!40000 ALTER TABLE `vehicle_option` DISABLE KEYS */;
+/*!40000 ALTER TABLE `vehicle_option` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `vehicle_vehicle_option`
+--
+
+DROP TABLE IF EXISTS `vehicle_vehicle_option`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `vehicle_vehicle_option` (
+  `articleid` int(9) unsigned NOT NULL,
+  `optionid` int(9) unsigned NOT NULL,
+  PRIMARY KEY (`articleid`,`optionid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `vehicle_vehicle_option`
+--
+
+LOCK TABLES `vehicle_vehicle_option` WRITE;
+/*!40000 ALTER TABLE `vehicle_vehicle_option` DISABLE KEYS */;
+/*!40000 ALTER TABLE `vehicle_vehicle_option` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -223,4 +498,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-05-13 16:14:44
+-- Dump completed on 2016-05-17 13:58:19
